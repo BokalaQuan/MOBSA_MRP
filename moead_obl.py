1d040a43930339e64ebab7cca4ae8920bd774890
BokalaQuan <quanwenming@outlook.com>
2017-12-27 12:34:19 +0800

    Data about mrp in 4 algorithms

--- moead_obl.py
+++ moead_obl.py
@@ -32,50 +32,49 @@ class MOEAD_OBL(MultiObjectiveEvolutionaryAlgorithmBasedOnDecomposition):
             self.current_population.append(sub_sol)
             self.update_archive(ind)
 
-    # def reproduction(self, ind):
-    #     fit = []
-    #     for i in ind.index_neighbor:
-    #         fit.append(SubProblem.cal_fit(self.current_population[i].solution,
-    #                                       ind.weight_vector, 'WS'))
+    def reproduction(self, ind):
+        fit = []
+        for i in ind.index_neighbor:
+            fit.append(SubProblem.cal_fit(self.current_population[i].solution,
+                                          ind.weight_vector, 'WS'))
+        #         # fit.append(SubProblem.cal_fit(self.current_population[i].solution,
+            #                               self.current_population[i].weight_vector, 'WS'))
     #
-    #         # fit.append(SubProblem.cal_fit(self.current_population[i].solution,
-    #         #                               self.current_population[i].weight_vector, 'WS'))
+        fit_worst = max(fit)
+        index_worst = fit.index(fit_worst)
+        sub_worst = self.current_population[index_worst]
+        ind_worst = sub_worst.solution
     #
-    #     fit_worst = max(fit)
-    #     index_worst = fit.index(fit_worst)
-    #     sub_worst = self.current_population[index_worst]
-    #     ind_worst = sub_worst.solution
+        ind_new = ind_worst.copy()
+        ind_new.mutation()
+        ind_new.fitness = ind_new.cal_fitness()
+        # fit_new = SubProblem.cal_fit(ind_new, ind.weight_vector,
+        #                              'TF', self.reference_point)
     #
-    #     ind_new = ind_worst.copy()
-    #     ind_new.mutation()
-    #     ind_new.fitness = ind_new.cal_fitness()
-    #     # fit_new = SubProblem.cal_fit(ind_new, ind.weight_vector,
-    #     #                              'TF', self.reference_point)
+        fit_new = SubProblem.cal_fit(ind_new, ind.weight_vector, 'WS')
+        # fit_new = SubProblem.cal_fit(ind_new, sub_worst.weight_vector, 'WS')
     #
-    #     fit_new = SubProblem.cal_fit(ind_new, ind.weight_vector, 'WS')
-    #     # fit_new = SubProblem.cal_fit(ind_new, sub_worst.weight_vector, 'WS')
+        if fit_new < fit_worst:
+            return ind_new
+        elif not ind_new >= ind_worst:
+            ind_new.mutation()
+            ind_new.fitness = ind_new.cal_fitness()
+            fit_new = SubProblem.cal_fit(ind_new, ind.weight_vector, 'WS')
+            # fit_new = SubProblem.cal_fit(ind_new, sub_worst.weight_vector, 'WS')
     #
-    #     if fit_new < fit_worst:
-    #         return ind_new
-    #     elif not ind_new >= ind_worst:
-    #         ind_new.mutation()
-    #         ind_new.fitness = ind_new.cal_fitness()
-    #         fit_new = SubProblem.cal_fit(ind_new, ind.weight_vector, 'WS')
-    #         # fit_new = SubProblem.cal_fit(ind_new, sub_worst.weight_vector, 'WS')
-    #
-    #         if fit_new < fit_worst:
-    #             return ind_new
-    #         else:
-    #             ind_new = ind_worst.copy()
-    #             ind_new.mutation()
-    #             ind_new.fitness = ind_new.cal_fitness()
-    #             return ind_new
-    #     else:
-    #         ind1 = ind_worst.copy()
-    #         ind2 = self.external_archive[random.randint(0, len(self.external_archive) - 1)].copy()
-    #         ind1.crossover(ind2)
-    #         ind1.fitness = ind1.cal_fitness()
-    #         return ind1
+            if fit_new < fit_worst:
+                return ind_new
+            else:
+                ind_new = ind_worst.copy()
+                ind_new.mutation()
+                ind_new.fitness = ind_new.cal_fitness()
+                return ind_new
+        else:
+            ind1 = ind_worst.copy()
+            ind2 = self.external_archive[random.randint(0, len(self.external_archive) - 1)].copy()
+            ind1.crossover(ind2)
+            ind1.fitness = ind1.cal_fitness()
+            return ind1
 
     def elite_opposition_based_learning(self):
         obl_list = []
